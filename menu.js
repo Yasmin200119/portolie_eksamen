ae18cfa6ddf8b5b810796a6b4e0da71aaa119a9c
Yasmin200119 <yagu0003@stud.ek.dk>
2026-01-06 12:43:56 +0100

    næsten done

--- menu.js
+++ menu.js
@@ -7,3 +7,12 @@ function burgerClick() {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
 }
+
+/* SLÅ LYD TIL/FRA PÅ VIDEO */
+const video = document.getElementById("video");
+const knap = document.getElementById("knap");
+
+knap.addEventListener("click", () => {
+  video.muted = !video.muted;
+  knap.textContent = video.muted ? "Slå lyd til" : "Slå lyd fra";
+});
